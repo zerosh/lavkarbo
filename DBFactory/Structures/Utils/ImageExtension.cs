121e2667310d5705cdfad346274a1a5063957d37
Stian Hagen <neutroniac@gmail.com>
2017-02-10 02:26:49 +0100

    Cleanup ImageExtension.

--- DBFactory/Structures/Utils/ImageExtension.cs
+++ DBFactory/Structures/Utils/ImageExtension.cs
@@ -48,14 +48,9 @@ namespace DBFactory.Structures
 
         private string FormatImage(byte[] imageByteArray)
         {
-            string FormattedImage = "DefaultImage.png";
-
             if (imageByteArray != null)
             {
-                var base64 = Convert.ToBase64String(imageByteArray);
-                FormattedImage = string.Format("data:image/gif;base64, {0}", base64);
-
-                return FormattedImage;
+                return string.Format("data:image/gif;base64, {0}", Convert.ToBase64String(imageByteArray));
             }
 
             return string.Empty;
